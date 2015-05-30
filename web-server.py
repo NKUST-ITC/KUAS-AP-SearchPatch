5e2e0b7bb99e0b7ece3e8a90c6f324ae507b2e93
JohnSounder <wnz9u3lb@gmail.com>
2015-05-30 22:40:50 +0800

    KUAS Lib Add
    KUAS Lib Add
    方便整理

--- web-server.py
+++ web-server.py
@@ -3,7 +3,7 @@
 import os
 import json
 import requests
-
+import kuas.ap as ap
 from lxml import etree
 from flask import Flask, render_template, request, session
 from flask_cors import *
@@ -12,8 +12,6 @@ ap_login_url = "http://140.127.113.231/kuas/perchk.jsp"
 fnc_url = "http://140.127.113.231/kuas/fnc.jsp"
 query_url = "http://140.127.113.231/kuas/%s_pro/%s.jsp"
 
-RANDOM_ID = "AG009"
-
 LOGIN_TIMEOUT = 5.0
 QUERY_TIMEOUT = 5.0
 RANDOM_TIMEOUT = 5.0
@@ -21,29 +19,8 @@ RANDOM_TIMEOUT = 5.0
 app = Flask(__name__)
 app.config.from_object("config")
 
-def login(session, username, password): #Login ---
-    payload = {"uid": username, "pwd": password}
-    r = session.post(ap_login_url, data=payload, timeout=LOGIN_TIMEOUT)
-    root = etree.HTML(r.text)
-    try:
-        is_login = not root.xpath("//script")[-1].text.startswith("alert")
-    except:
-        is_login = False
-    return is_login
-
-def query(session, qid=None, args=None): #Query for all type
-    payload = {} #New a payload and this will be the post data
-    for key in args:
-        payload[key] = args[key]
-    try:
-        r = session.post(query_url % (qid[:2], qid), data=payload, timeout=QUERY_TIMEOUT).content
-    except requests.exceptions.ReadTimeout:
-        r = ""
-    return r
-
 @app.route('/ap/logout', methods=['POST'])
 @cross_origin(supports_credentials=True)
-
 def logout():
     session.clear()
     return 'logout'
@@ -55,8 +32,8 @@ def query_classroom(): #This is a query for class room
         yms_yms = request.form['yms_yms']
         room_id = request.form['room_id']
         s = requests.session()
-        login(s, "guest", "123")
-        return query(s, "ag302_02", {"yms_yms":yms_yms,"room_id":room_id,"unit_serch":"查 詢"})
+        ap.login(s, "guest", "123")
+        return ap.query(s, "ag302_02", {"yms_yms":yms_yms,"room_id":room_id,"unit_serch":"查 詢"})
     return render_template("query.html")
 
 @app.route('/ap/query_class', methods=['GET', 'POST', 'OPTIONS'])
@@ -68,12 +45,9 @@ def query_department(): #This can dump the department list
         arg01 = yms_yms.split('#')[0]
         arg02 = yms_yms.split('#')[1]
         s = requests.session()
-        login(s, "guest", "123")
-        return query(s, "ag304_03", {"arg01": arg01, "arg02": arg02, "arg": arg})
+        ap.login(s, "guest", "123")
+        return ap.query(s, "ag304_03", {"arg01": arg01, "arg02": arg02, "arg": arg})
     return render_template("query_class.html")
-
-#def query_class(): #This can query the class
-#    if request.method == "POST":
         
 if __name__ == '__main__':
     app.run(host="127.0.0.1")
